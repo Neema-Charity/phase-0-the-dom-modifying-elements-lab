ef29a965e7ce93e4ef5285f9c798d06753b12c2d
Neema-Charity <ncharityyy@gmail.com>
2024-04-07 20:01:09 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main#main').remove()
+
+const newHeader = document.createElement('H1')
+newHeader.nodeName
+newHeader.id = 'victory'
+newHeader.textContent = 'Nathan is the champion'
